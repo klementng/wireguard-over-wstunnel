23f004443ad3baf68cd90ac79354ce6459d0543a
clement <clement.ng.62@outlook.com>
2023-07-26 01:38:07 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,257 @@
+import socket
+import os
+import ipaddress
+import ctypes
+import atexit
+import subprocess
+import sys
+import time
+import argparse
+import wgconfig
+import yaml
+import tempfile
+import logging
+import shutil
+import requests
+import psutil
+import ctypes
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger()
+
+
+## global variables
+
+config = None
+
+##
+
+
+def configure_wstunnel():
+    logger.info("Loading wstunnel config...")
+
+    proto, host = config['wstunnel'].get('server').split("://")
+    localToRemote = config['wstunnel'].get('localToRemote').split(':')
+
+    if len(localToRemote) == 3:
+        listen_port = localToRemote[0]
+        listen_ip = '0.0.0.0'
+    else:
+        listen_port = localToRemote[1]
+        listen_ip = localToRemote[0]
+
+    endpoint_ip = socket.gethostbyname(host)
+
+    # change server to ip / host
+    config['wstunnel']['server'] = f"{proto}://{endpoint_ip}"
+    config['wstunnel'].setdefault('hostHeader', host)
+
+    logger.info(f"Setting wstunnel to listen on: {listen_ip}:{listen_port}")
+    logger.info(
+        f"Setting wstunnel endpoint to: {config['wstunnel']['server']}")
+    logger.info(
+        f"Setting wstunnel host header to: {config['wstunnel']['hostHeader']}")
+
+    return endpoint_ip, listen_ip, listen_port
+
+
+def configure_wireguard(wst_endpoint_ip, wst_local_ip, wst_local_port):
+    logger.info("Loading wireguard config...")
+
+    tmpfile = tempfile.NamedTemporaryFile(
+        'w+', prefix='wg-wst-', suffix='.conf', delete=False)
+    logger.debug(f"Created temporary file at {tmpfile.name}")
+
+    if config['wireguard'].get('path'):
+
+        if config['wireguard'].get('str') != None:
+            logger.warning(
+                "wireguard 'path' is set. The 'str' config is ignored")
+
+        logger.info(f"Using wireguard file at: {config['wireguard']['path']}")
+        with open(config['wireguard']['path']) as f:
+            tmpfile.write(f.read())
+
+    else:
+        logger.info(f"Using wireguard config str")
+        tmpfile.write(config['wireguard']['str'])
+
+    tmpfile.close()
+
+    wg_config = wgconfig.WGConfig(tmpfile.name)
+    wg_config.read_file()
+
+    allowed_ips = []
+    peer_id = list(wg_config.peers.keys())[0]
+
+    logger.info(f"Allowing outgoing connection to {wst_endpoint_ip}")
+
+    for ips in wg_config.peers[peer_id]["AllowedIPs"]:
+        try:
+            net = ipaddress.ip_network(ips)
+            allowed_ips.extend(
+                map(str, net.address_exclude(
+                    ipaddress.ip_network(wst_endpoint_ip + "/32")
+                ))
+            )
+        except TypeError:
+            allowed_ips.append(ips)
+
+    allowed_ips = ", ".join(set(allowed_ips))
+    wg_config.del_attr(peer_id, "AllowedIPs")
+    wg_config.add_attr(peer_id, "AllowedIPs", allowed_ips)
+
+    if wst_local_ip == '0.0.0.0':
+        wst_local_ip = '127.0.0.1'
+
+    logger.info(
+        f"Changing wireguard endpoint ip to {wst_local_ip}:{wst_local_port}")
+    wg_config.del_attr(peer_id, "Endpoint")
+    wg_config.add_attr(peer_id, "Endpoint", f"{wst_local_ip}:{wst_local_port}")
+
+    wg_config.write_file(tmpfile.name)
+
+    return tmpfile, wg_config
+
+
+def start_wireguard(wg_config_path):
+    interface_name = os.path.basename(wg_config_path).replace('.conf', '')
+
+    logger.info("Starting wireguard...")
+    logger.info(f"Wireguard interface name: {interface_name}")
+
+    if config['app']['os'] == 'windows':
+        logger.warning(f"Untunneled connection are likely not blocked on windows. \
+            You will need to restart any application previously connected to the internet to utilize wireguard vpn")
+        
+        os.system("\"%s\" /installtunnelservice %s" %
+                  (config["app"]["wireguard_path"], wg_config_path))
+    else:
+        os.system("%s up %s" %
+                  (config["app"]["wireguard_path"], wg_config_path))
+
+
+def stop_wireguard(wg_config_path):
+    interface_name = os.path.basename(wg_config_path).replace('.conf', '')
+    logger.info(f"Stopping wireguard interface: {interface_name}")
+
+    if config['app']['os'] == 'windows':
+        os.system("\"%s\" /uninstalltunnelservice %s" %
+                  (config["app"]["wireguard_path"], interface_name))
+    else:
+        os.system("%s down %s" %
+                  (config["app"]["wireguard_path"], wg_config_path))
+
+    time.sleep(1)
+
+
+def start_wstunnel():
+    logger.info("Starting wstunnel...")
+    wst_config = config['wstunnel']
+
+    wst_args = [config['app']['wstunnel_path']]
+    wst_host = wst_config.pop('server')
+    for i in wst_config.keys():
+
+        flag = '-' if len(i) == 1 else '--'
+
+        if isinstance(wst_config[i], bool):
+            if wst_config[i] == True:
+                wst_args.append(f"{flag}{i}")
+        else:
+            flag = '-' if len(i) == 1 else '--'
+            wst_args.append(f"{flag}{i}={wst_config[i]}")
+
+    wst_args.append(wst_host)
+
+    return subprocess.Popen(wst_args)
+
+
+def stop_wstunnel(process: subprocess.Popen):
+    logger.info("Stopping wstunnel")
+    return process.kill()
+
+
+def cleanup_tmpfile(tmpfile):
+    logger.info(f"Removing temp file at {tmpfile.name}")
+    os.unlink(tmpfile.name)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Wireguard over wstunnel")
+    parser.add_argument('--action', '-a', help="Start the program / clean wg tunnel that are not properly stopped",
+                        default='start', choices=['start', 'clean'])
+    parser.add_argument(
+        '--config', '-c', help="Path to program config", default='./config.yml')
+    args = parser.parse_args()
+
+    logger.info("Loading app config...")
+    with open(args.config) as f:
+        config = yaml.full_load(f)
+
+    # Force elevation
+    if config['app']['os'] == 'windows':
+        if not ctypes.windll.shell32.IsUserAnAdmin():
+            ctypes.windll.shell32.ShellExecuteW(
+                None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+            exit(0)
+
+    elif config['app']['os'] == 'linux':
+        if os.geteuid() != 0:
+            logger.fatal("You need to run this program as root / sudo")
+            exit(1)
+
+    else:
+        logger.fatal("Unsupported Platform")
+        exit(1)
+
+    # disconnect old connections
+    for i in psutil.net_if_addrs().keys():
+        if i.startswith('wg-wst'):
+            stop_wireguard(i)
+
+    if args.action == 'clean':
+        input("Press any key to exit...")
+        exit(0)
+
+    wst_endpoint_ip, wst_listen_ip, wst_listen_port = configure_wstunnel()
+    tmpfile, wg_config = configure_wireguard(
+        wst_endpoint_ip, wst_listen_ip, wst_listen_port)
+
+    try:
+
+        if config['app']['start_wireguard'] == True:
+            start_wireguard(tmpfile.name)
+            atexit.register(stop_wireguard, tmpfile.name)
+
+        if config['app']['start_wstunnel'] == True:
+            wst_process = start_wstunnel()
+            atexit.register(stop_wstunnel, wst_process)
+
+        if config['app']['export_wireguard_conf'] == True:
+            shutil.copyfile(tmpfile.name, os.path.basename(tmpfile.name))
+
+        atexit.register(cleanup_tmpfile, tmpfile)
+
+        while True:
+            time.sleep(3)
+            res = requests.get("https://api.ipify.org")
+
+            if res.status_code == 200:
+                logger.info(f"Your Public IP is: {res.text}")
+                break
+
+            else:
+                logger.warning("Health Check failed!")
+
+        logger.info(f"Press CTRL + C to exit")
+        while True:
+            pass
+
+    except KeyboardInterrupt:
+        logger.info(f"Exiting...")
+
